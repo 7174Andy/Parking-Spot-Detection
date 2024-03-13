d993734c52a4adf789a96b6f1ef8e7c9407e207f
7174Andy <hep003@ucsd.edu>
2024-03-13 11:16:49 -0700

    add helper functions to extract features

--- dataSetup.py
+++ dataSetup.py
@@ -7,34 +7,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 import shutil
 
-input_path = "datasets/coco-2017"
-output_path = "datasets/yolo-datasets"
+input_path = "datasets/coco-2017/validation"
+output_path = "datasets/yolo-datasets/images"
 
 file_names = []
+f = open("datasets/coco-2017/validation/labels.json")
+data = json.load(f)
+f.close()
 
 
+# Load the images from the folder
 def load_images_from_folder(folder):
-    count = 0
     for filename in os.listdir(folder):
         source = os.path.join(folder, filename)
-        destination = f"{output_path}/images/{filename}"
-
+        destination = f"{output_path}/{filename}"
         try:
             shutil.copy(source, destination)
-            print(f"Image {filename} copied")
-        except:
-            print(f"Image {filename} already exists")
-            continue
+            print(f"Copying {source} to {destination}")
+        except shutil.SameFileError:
+            print(f"Failed to copy {source} to {destination}")
 
         file_names.append(filename)
-        count += 1
+
+
+def get_img_annotation(img_id):
+    img_annotation = []
+    isFound = False
+    for ann in data["annotations"]:
+        if ann["image_id"] == img_id:
+            img_annotation.append(ann)
+            isFound = True
+    if isFound:
+        return img_annotation
+    else:
+        return None
 
 
 def download_coco_dataset():
     # Download the COCO 2017 Dataset
     dataset = foz.load_zoo_dataset(
         "coco-2017",
-        split="validation",
+        splits=["validation", "training"],
         label_types=["detections"],
         dataset_dir="./datasets/coco-2017",
         max_samples=1000,
@@ -47,9 +60,14 @@ def download_coco_dataset():
     session.dataset = dataset
 
 
+def get_img_by_filename(filename):
+    for img in data["images"]:
+        if img["file_name"] == filename:
+            return img
+
+
 def main():
-    load_images_from_folder("datasets/coco-2017/validation")
-    print(file_names)
+    print(data["images"][0])
 
 
 if __name__ == "__main__":
