fac13f58170aa6de252cdb3ed778097c38bb08fe
7174Andy <hep003@ucsd.edu>
2024-03-13 11:56:24 -0700

    completed the code for COCO to YOLO conversion

--- dataSetup.py
+++ dataSetup.py
@@ -66,8 +66,59 @@ def get_img_by_filename(filename):
             return img
 
 
+def convert_labels():
+    load_images_from_folder(input_path + "/data")
+    print("Finished loading images from the folder")
+    for filename in file_names:
+        # Extract images
+        image = get_img_by_filename(filename)
+        img_id = image["id"]
+        img_width = image["width"]
+        img_height = image["height"]
+
+        # Get annotations
+        img_annotation = get_img_annotation(img_id)
+        print(f"Processing {filename}...")
+
+        if img_annotation:
+            file_label = open(f"datasets/yolo-datasets/labels/{filename}.txt", "w")
+
+            for ann in img_annotation:
+                current_category = ann["category_id"] - 1
+                current_bbox = ann["bbox"]
+                x = current_bbox[0]
+                y = current_bbox[1]
+                box_width = current_bbox[2]
+                box_height = current_bbox[3]
+
+                # Get the center of the bounding box
+                x_center = x + (x + box_width) / 2
+                y_center = y + (y + box_height) / 2
+
+                # Normalization
+                x_center = x_center / img_width
+                y_center = y_center / img_height
+                box_width = box_width / img_width
+                box_height = box_height / img_height
+
+                # Limiting the values with 6 decimal figures
+                x_center = format(x_center, ".6f")
+                y_center = format(y_center, ".6f")
+                box_width = format(box_width, ".6f")
+                box_height = format(box_height, ".6f")
+
+                # Write those data into the new label.txt file
+                file_label.write(
+                    f"{current_category} {x_center} {y_center} {box_width} {box_height}\n"
+                )
+
+            print(f"Finished writing {filename}.txt")
+            file_label.close()
+
+
 def main():
-    print(data["images"][0])
+    # download_coco_dataset()
+    convert_labels()
 
 
 if __name__ == "__main__":
